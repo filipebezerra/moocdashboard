3fb77099f414006d2db72c2d5545a81f432aea2c
Filipe Bezerra <filipebzerra@gmail.com>
2014-10-28 18:01:44 -0200

    Adjusting the form search

--- js/scripts.js
+++ js/scripts.js
@@ -17,11 +17,15 @@ $(document).ready(function() {
     $("#menu-toggle").click(function(e) {
         e.preventDefault();
         var elem = document.getElementById("sidebar-wrapper");
+        var formSearch = document.getElementById("form-search");
         left = window.getComputedStyle(elem,null).getPropertyValue("left");
         if(left == "200px"){
             document.getElementsByClassName("sidebar-toggle")[0].style.left="-200px";
+            // TODO include effect to wait the sidebar close
+            formSearch.hidden = false;
         }
         else if(left == "-200px"){
+            formSearch.hidden = true;
             document.getElementsByClassName("sidebar-toggle")[0].style.left="200px";
         }
     });
